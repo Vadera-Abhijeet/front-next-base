9dfe396e5e542f6b820500e27f805650aee6a351
Vishal Akbari <vishal@scaletech.xyz>
2024-02-21 16:18:22 +0530

    Added eslint and husky

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	// this will check Typescript files
-	'**/*.(ts|tsx)': () => 'npm run tsc --noEmit',
+	'**/*.(ts|tsx)': () => 'tsc --noEmit',
 
 	// This will lint and format TypeScript and                                             //JavaScript files
 	'**/*.(ts|tsx|js)': (filenames) => [
-		`npm run eslint --fix ${filenames.join(' ')}`,
-		`npm run prettier --write ${filenames.join(' ')}`
+		`eslint --fix ${filenames.join(' ')}`,
+		`prettier --write ${filenames.join(' ')}`
 	],
 
 	// this will Format MarkDown and JSON
-	'**/*.(md|json)': (filenames) => `npm run prettier --write ${filenames.join(' ')}`
+	'**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`
 };
